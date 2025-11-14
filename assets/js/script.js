1b08d1b93e2b0cfe6e291dfbc2270da208dc6d9c
BenjaTriple7 <be.sanchezg@duocuc.cl>
2025-11-14 19:04:21 -0300

    header css y script

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+
+
+const searchBtn = document.querySelector('.search-btn');
+const searchContainer = document.querySelector('.search-container');
+
+searchBtn.addEventListener('click', () => {
+    searchContainer.classList.toggle('active');
+});
