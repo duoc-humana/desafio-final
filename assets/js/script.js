ca97e3eb0a2175ddf25e8fa1f6ae3e753631696a
BenjaTriple7 <be.sanchezg@duocuc.cl>
2025-11-17 20:38:37 -0300

    Detalles filtro

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,10 @@ const searchContainer = document.querySelector('.search-container');
 searchBtn.addEventListener('click', () => {
     searchContainer.classList.toggle('active');
 });
+
+
+/* slide  */
+
+document.getElementById("filterToggle").addEventListener("click", function () {
+    document.getElementById("filterAside").classList.toggle("open");
+});
